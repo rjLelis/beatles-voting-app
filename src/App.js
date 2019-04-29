ca0d7d8a07a6a48616e0f98e85d1bd937d1411a9
rjLelis <renatojlelis@gmail.com>
2019-04-29 16:14:53 -0300

    Finish tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const list = [
+    {
+      id: 1,
+      name: 'John Lennon',
+      votes: 0
+    },
+    {
+      id: 2,
+      name: 'Paul McCartney',
+      votes: 0
+    },
+    {
+      id: 3,
+      name: 'George Harrison',
+      votes: 0
+    },
+    {
+      id: 4,
+      name: 'Ringo Starr',
+      votes: 0
+    }
+  ]
+
+
+  class App extends Component {
+
+    state = {
+      members: []
+    }
+
+    componentDidMount(){
+      this.setState({ members: list })
+    }
+
+    handleClick = memberId => {
+      const updatedList = this.state.members.map(member =>{
+        if(member.id === memberId) {
+          return Object.assign({}, member, {
+            votes: member.votes + 1
+          })
+        } else {
+          return member
+        }
+      })
+      this.setState({
+        members: updatedList 
+      })
+    }
+
+    render() {
+      return(
+        this.state.members.map(member =>
+          <Beatle key={member.id} name={member.name} id={member.id} votes={member.votes} onVote={this.handleClick} />
+        )
+        
+      )
+    }
+  }
+
+  class Beatle extends Component {
+
+    onClickHandle = () => this.props.onVote(this.props.id)
+    
+    render() {
+      return (
+        <div className='App'>
+          {this.props.name} <button onClick={this.onClickHandle}>+</button>
+          {this.props.votes}
+        </div>
+      )
+    }
+  }
+
+export default App
